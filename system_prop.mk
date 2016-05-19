e60639e58dc26b4ed8c1a58d55071419ba5c8e0c
kyasu <kyasu.dev@gmail.com>
2016-05-19 13:21:28 +0900

    hltedcm: Add HltedcmRIL which enables MOBILE_DATA_ALWAYS_ON by default

--- system_prop.mk
+++ system_prop.mk
@@ -69,7 +69,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.forced_succ_ack=false \
     persist.radio.default_wappush=true \
     persist.data.qmi.adb_logmask=0 \
-    ro.telephony.ril_class=SamsungQcomRIL \
+    ro.telephony.ril_class=HltedcmRIL \
     ro.telephony.default_network=9 \
     telephony.lteOnGsmDevice=1
 
