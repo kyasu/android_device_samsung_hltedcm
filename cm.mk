ee5a6e4007b98469254192374f8020bc81f8aca6
kyasu <kyasu.dev@gmail.com>
2016-11-19 10:03:20 +0900

    hltedcm: Remove enhanced NFC feature comment

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/hltedcm/full_hltedcm.mk)
 
-# Enhanced NFC
-#$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
