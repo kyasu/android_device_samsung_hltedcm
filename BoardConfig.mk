kyasu/android_device_samsung_js01lte

js01lte: sepolicy: Rewrite for O
* Kang from klte-common

BoardConfig.mk
@@ -158,8 +158,7 @@ BOARD_GLOBAL_CFLAGS += -DTW_USE_MINUI_CUSTOM_FONTS
 endif
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/samsung/js01lte/sepolicy
+include device/samsung/js01lte/sepolicy/sepolicy.mk
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true

sepolicy/sepolicy.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/qcom/sepolicy/sepolicy.mk
+-include device/qcom/sepolicy/legacy-sepolicy.mk
+
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/js01lte/sepolicy/common
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung/js01lte/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/samsung/js01lte/sepolicy/private

kyasu/android_device_samsung_hltedcm

BoardConfig.mk
@@ -158,8 +158,7 @@ BOARD_GLOBAL_CFLAGS += -DTW_USE_MINUI_CUSTOM_FONTS
 endif
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/samsung/hltedcm/sepolicy
+include device/samsung/hltedcm/sepolicy/sepolicy.mk
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true

sepolicy/sepolicy.mk
@@ -1,0 +1,28 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/qcom/sepolicy/sepolicy.mk
+-include device/qcom/sepolicy/legacy-sepolicy.mk
+
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/hltedcm/sepolicy/common
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/samsung/hltedcm/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/samsung/hltedcm/sepolicy/private