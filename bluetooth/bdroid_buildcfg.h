d1629b4b993d99813a3841e7da398daf2659fa52
kyasu <kyasu.dev@gmail.com>
2019-02-28 15:07:41 +0900

    hltedcm: Set default hotspot name
    * Make default bluetooth name same

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Note 3"
+#define BTM_DEF_LOCAL_NAME   "Galaxy Note 3"
 
 #define BTA_DISABLE_DELAY 100 /* in milliseconds */
 #define BLE_VND_INCLUDED TRUE
